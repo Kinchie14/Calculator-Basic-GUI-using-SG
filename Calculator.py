9c8547c1c26e4ec1c2dd2025db30637fffb77d1c
Vincent Vargas <vincentvargas6969@gmail.com>
2022-08-02 19:41:01 -0700

    Initializing Buttons

--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,31 @@
+from re import L
 import PySimpleGUI as sg
 
+
+
+
+layout = [[sg.Text('Output')],
+
+
+[sg.Button('Enter'), sg.Button(' Clear')],
+
+[sg.Button(7), sg.Button(8), sg.Button(9), sg.Button('/')],
+
+[sg.Button(4), sg.Button(5), sg.Button(6), sg.Button('*')],
+
+[sg.Button(1), sg.Button(2), sg.Button(3), sg.Button('-')],
+
+[sg.Button(0), sg.Button('.'), sg.Button('+')]
+
+
+]
+
+window = sg.Window('Calculator', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+
+window.close()
